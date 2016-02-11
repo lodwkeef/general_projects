6682d1735dc806d62b6e76eaa4b769d9002cad9d
RockemSockemRobots <jsiekmann@email.arizona.edu>
2016-02-10 20:50:32 -0700

    timer files

--- lab1p2.X/timer.c
+++ lab1p2.X/timer.c
@@ -8,8 +8,17 @@
 #include <xc.h>
 #include "timer.h"
 
-void delayUs(unsigned int delay){
+void initTimer2(){
+    TMR2 = 0;
+    T2CONbits.TCKPS = 0; //1 prescalar value 
+    IPC2bits.T2IP = 7;
+    IFS0bits.T2IF = 0;
+    IEC0bits.T2IE = 1;
+    T2CONbits.ON = 0;
+}
 
+void delayUs(unsigned int delay){
     //TODO: Create a delay for "delay" micro seconds using timer 2
-    
+    PR2 = 10*delay - 1;
+    T2CONbits.ON = 1;
 }
