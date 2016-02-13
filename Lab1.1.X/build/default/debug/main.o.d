9afdafd55b2f5b9cd266ffd3748d675f4f3d0bef
RockemSockemRobots <jsiekmann@email.arizona.edu>
2016-02-12 23:15:17 -0700

    adding part1 cuz it felt lonely and excluded

---
+++ Lab1.1.X/build/default/debug/main.o.d
@@ -0,0 +1,7 @@
+build/default/debug/main.o:  \
+main.c  \
+config.h  \
+leds.h  \
+interrupt.h  \
+switch.h  \
+ timer.h 
---
+++ Lab1.1.X/leds.h
@@ -0,0 +1,17 @@
+/* 
+* File:   leds.h
+* Author: 
+*
+* Created on December 27, 2014, 1:31 PM
+*/
+
+#ifndef LEDS_H
+#define	LEDS_H
+#define OUTPUT 0
+#define INPUT 1
+
+
+void initLEDs();
+void turnOnLED(int led);//function to turn on LEDs
+#endif	/* LEDS_H */
+
