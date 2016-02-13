561fca8f59ff044efcb60e5963017e9fe7bda7c0
lodwkeef <kevincurtis@email.arizona.edu>
2016-02-12 18:24:29 -0700

    combining lab 1 parts 1 and 2 to begin building up part3

--- lab1p3.X/main.c
+++ lab1p3.X/main.c
@@ -1,6 +1,6 @@
 // ******************************************************************************************* //
 //
-// File:         lab1p2.c
+// File:         lab1p3.c
 // Date:         12-30-2014
 // Authors:      Garrett Vanhoy
 //
@@ -47,15 +47,64 @@ int main(void)
     initTimer2();
     initLCD();
     initTimer1(); //Initialize Timer1
-    initSWext(); //Initialize SW1
+    initSWext(); //Initialize external Switch
     
     while(1)
     {
         
-        
+        switch (state) {
+            
+            case RUN_LED:
+                turnOnLED(RUN);
+                currLED = RUN;
+                T1CONbits.TON = 0;//Timer is disabled
+                TMR1 = 0;
+                next = PRESS;
+                break;
+                
+            case STOP_LED:
+                turnOnLED(STOP); //CHECK TO SEE IF I AM REUSING VARIABLES IMPROPERLY
+                currLED = STOP;
+                T1CONbits.TON = OFF;//Timer is disabled
+                TMR1 = 0; 
+                next = PRESS;
+                break;
+                
+            case debounce:
+                T1CONbits.TON = ON;//Timer is enabled              
+                break;
+                
+            case press:
+                T1CONbits.TON = OFF;//Timer is disabled
+                TMR1 = 0;
+                next = RELEASE;
+                break;
+        }
         //testTimer2();
         //testLCD();
     }
     
     return 0;
 }
+
+
+void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt() {
+    int dummy;
+    dummy = PORTA; // Read the Port A
+    IFS1bits.CNAIF = 0; //Set flag to lowered
+
+    TMR1 = 0; //reset timer 1
+
+
+    if (state == STOP_LED || state == RUN_LED || state == press) state = debounce;
+    
+}
+
+void __ISR(_TIMER_1_VECTOR, IPL7SRS) _T1Interrupt() {
+    IFS0bits.T1IF = 0; //lower the flag 
+    TMR1 = 0;
+    if(next == PRESS) state = press;
+    else if(next != PRESS && currLED == STOP) state = RUN_LED;
+    else if(next != PRESS && currLED == RUN) state = STOP_LED;
+}
+
