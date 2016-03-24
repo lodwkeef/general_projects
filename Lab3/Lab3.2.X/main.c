854845fcdfb236fe471998b727fafa7fdae9effc
lodwkeef <kevincurtis@email.arizona.edu>
2016-03-24 12:20:40 -0700

    some updates, cleaningish, debounce not touched

--- Lab3/Lab3.2.X/main.c
+++ Lab3/Lab3.2.X/main.c
@@ -27,18 +27,22 @@
 #define ON 1
 #define M1 1
 #define M2 2
+#define YES 1
+#define NO 0
+#define FRWD 1
+#define BKWD 0
 
 typedef enum stateTypeEnum {
     forward, idle, backward,
 } stateType;
 
-float dispVolt = 0;
-int direction = 1;//forward
+//float dispVolt = 0;   //this should just me in main -kdc
+volatile int direction = FRWD;//forward
 volatile stateType state = forward;
 volatile stateType prevstate = forward;
 volatile int change = 0;
-volatile int remap = 0;
-float ADCbuffer = 0;
+volatile int remap = NO;
+//float ADCbuffer = 0;  //this should just be in main -kdc
 //*************************************************************************8****************** //
 
 int main(void){
@@ -51,30 +55,33 @@ int main(void){
     initLCD();
     initPWM();
     initADC();
+    float ADCbuffer = 0;
+    float dispVolt = 0;
         
-    setMotorDirection(M1, 1); 
-    setMotorDirection(M2, 1);
-    while(1){      //Lab3 Part1
+    setMotorDirection(M1, FRWD); 
+    setMotorDirection(M2, FRWD);
+    while(1){      //Lab3 Part2
        switch(state){
             case forward:
                 prevstate = forward; 
                 ADCbuffer = getADCbuffer();
-                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
-                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
+                dispVolt = UpdateLCDVoltage(dispVolt, ADCbuffer);
+//                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
+//                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
 //                dispVolt = UpdateLCDVoltage(dispVolt);
                 moveCursorLCD(1,0);
                 printStringLCD("forward");
-                if(remap == 1){
-                    setMotorDirection(M1,1);
-                    setMotorDirection(M2,1);
+                if(remap == YES){
+                    setMotorDirection(M1,FRWD);
+                    setMotorDirection(M2,FRWD);
                     delayUs(1000);
-                    remap = 0;
+                    remap = NO;
                 }
                 setMotorSpeed(ADCbuffer, direction);
                 break;
@@ -82,36 +89,38 @@ int main(void){
             case backward:
                 prevstate = backward;
                 ADCbuffer = getADCbuffer();
-                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
-                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
+                dispVolt = UpdateLCDVoltage(dispVolt, ADCbuffer);
+//                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
+//                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
                 moveCursorLCD(1,0);
                 printStringLCD("backward");
-                if(remap == 1){
-                    setMotorDirection(M1,0);
-                    setMotorDirection(M2,0);
+                if(remap == YES){
+                    setMotorDirection(M1,BKWD);
+                    setMotorDirection(M2,BKWD);
                     delayUs(1000);
-                    remap = 0;
+                    remap = NO;
                 }
                 setMotorSpeed(ADCbuffer, direction);
                 break;           
 
-            case idle:
+            case idle:          //state between directions, 
                 unmapPins();
                 ADCbuffer = getADCbuffer();
-                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
-                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
-                    printVoltage(ADCbuffer);
-                    dispVolt = ADCbuffer;
-                }
+                dispVolt = UpdateLCDVoltage(dispVolt, ADCbuffer);
+//                if((dispVolt < ADCbuffer) && ((dispVolt + 1) <= ADCbuffer)){//to reduce excessive LCD updates
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
+//                else if((dispVolt > ADCbuffer) && ((dispVolt - 1) >= ADCbuffer)){
+//                    printVoltage(ADCbuffer);
+//                    dispVolt = ADCbuffer;
+//                }
                 moveCursorLCD(1,0);
                 printStringLCD("Idle");
                 delayUs(1000);
@@ -125,11 +134,11 @@ int main(void){
 void __ISR(_CHANGE_NOTICE_VECTOR, IPL7SRS) _CNInterrupt(){
     int dummy;
     dummy = PORTA; // Read the Port A
-    T1CONbits.TON = ON;
+    T1CONbits.TON = ON; //turn on debounce timer
     if(change == 2){
         if((state == forward) || (state == backward)) state = idle; 
-        else if((state == idle) && (prevstate == forward)){ state = backward; direction = 0; remap = 1;}
-        else if((state == idle) && (prevstate == backward)){ state = forward; direction = 1; remap = 1;}
+        else if((state == idle) && (prevstate == forward)){ state = backward; direction = BKWD; remap = YES;}
+        else if((state == idle) && (prevstate == backward)){ state = forward; direction = FRWD; remap = YES;}
         change = 0;
     }
     IFS1bits.CNAIF = 0; //lower flag    
