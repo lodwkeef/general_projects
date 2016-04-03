50b172f50aaa23c19e02671bfaf9a508fda443ba
bschifman <bschifman@email.arizona.edu>
2016-04-03 15:52:51 -0700

    adc.c updated

--- Final_Project/Final_Project.X/adc.c
+++ Final_Project/Final_Project.X/adc.c
@@ -15,23 +15,30 @@
 #include <xc.h>
 #include "adc.h"
 
-void initADC(){
-    //the tristate for the analog pin. What is up with that doc
+void initADC(){ //updated for Final Project
     TRISBbits.TRISB0 = 1; //set pin to input 
+    TRISBbits.TRISB1 = 1;
+    TRISBbits.TRISB2 = 1;
+    TRISBbits.TRISB3 = 1;
+    
     ANSELBbits.ANSB0 = 1; // RB0 set to analog
+    ANSELBbits.ANSB1 = 1;
+    ANSELBbits.ANSB2 = 1;
+    ANSELBbits.ANSB3 = 1;
+    
     AD1CON1bits.FORM = 0; // 16 unsigned integer
     AD1CON1bits.SSRC = 7; // Auto-convert mode
     AD1CON1bits.ASAM = 1; // Auto-sampling
     AD1CON2bits.VCFG = 0; // Use board refernece voltages
-    AD1CON2bits.CSCNA = 0; // Disable scanning
-    AD1CON2bits.SMPI = 15; // 15 burritos
+    AD1CON2bits.CSCNA = 1; //UPDATED: scan different inputs (was 0)
+    AD1CON2bits.SMPI = 15; // 16 burritos
     AD1CON2bits.ALTS = 0; // Only Mux A
     AD1CON3bits.ADRC = 0; // Use PBCLK
     AD1CON3bits.SAMC = 2; // 2 Tad per sample
     AD1CON3bits.ADCS = 0xFF; // 512 times the PBCLK
     AD1CHSbits.CH0NA = 0; // Use Vref- as negative reference
-    AD1CHSbits.CH0SA = 0; // Scan AN0 at least
-    AD1CSSLbits.CSSL0 = 1; //ADC pin select bits, may not change anything -BAS
+    AD1CHSbits.CH0NA = 0; //UPDATED: Scan is enabled, these are ignored
+    AD1CSSLbits.CSSL0 = 0x0000000F; //UPDATED: Scanning AN0-AN3
     IFS0bits.AD1IF = 0;
     IEC0bits.AD1IE = 1;
     IPC5bits.AD1IP = 7;
